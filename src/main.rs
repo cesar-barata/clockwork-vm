9b6aa7a6ad9ded7175a804b9aea39694cb7057b7
Cesar Barata <cesar.adbarata@gmail.com>
2019-08-01 22:25:22 -0300

    Added "load" instruction and "add" instruction stub

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,52 @@ mod kinematic {
 
 	type Word = u64;
 
-	const OPCODE_OFFSET: usize = 60;
+	const OPCODE_OFFSET: usize = 10;
 
 	#[derive(Debug, PartialEq)]
-	enum Opcode {
+	enum Instruction {
 		Illegal,
 		Halt,
-		OldLoad,
-		Load { a: Word },
+		Load { value: Word, dest_reg: u8 },
+        Add { src1: u8, src2: u8, dest: u8  }
 	}
 
-	impl From<Word> for Opcode {
-		fn from(value: Word) -> Opcode {
-			//println!("{}", value >> 60);
-			match value {
-				0 => Opcode::Halt,
-				1 => Opcode::OldLoad,
-				x if x > 1024 => Opcode::Illegal,
-				_ => Opcode::Illegal
+    impl Instruction {
+        fn parse_load(operands: u64) -> Self {
+            // TODO change operand order: DEST-VALUE-OPCODE
+            // load layout: 0b00000000000000000000000000000000000000000000000_00000000(_0000000000)
+            //                                  VALUE                           DEST     OPCODE
+            let dest_reg = operands as u8;
+            let value = (operands >> 8) as u64;
+            Instruction::Load { value, dest_reg }
+        }
+
+        fn parse_add(operands: u64) -> Self {
+            // add layout: 0b000000000000000000_000000000000000000_000000000000000000(_0000000000)
+            //                     DEST               SRC2               SRC1           OPCODE
+            let src1 = operands as u8;
+            let src2 = (operands >> 18) as u8;
+            let dest = (operands >> 36) as u8;
+            Instruction::Add { src1, src2, dest }
+        }
+    }
+
+	impl From<Word> for Instruction {
+		fn from(instruction: Word) -> Self {
+            let opcode = (instruction & 0b0000001111111111) as u16;
+            let operands = (instruction >> OPCODE_OFFSET) as u64;
+			match opcode {
+				0 => Instruction::Halt,
+                1 => Self::parse_load(operands),
+                2 => Self::parse_add(operands),
+				x if x > 1024 => Instruction::Illegal, // we have only 2.pow(10) = 1024 opcode slots
+				_ => Instruction::Illegal              // for still unimplemented instructions
 			}
 		}
-	}
+    }
 
 	pub struct Kinematic {
+        // TODO represent writable registers as array
 		da: i64,
 		db: i64,
 		dc: i64,
@@ -65,20 +88,39 @@ mod kinematic {
 		fn step(&mut self) -> bool {
 			let instruction = &self.fetch_next_instr();
 
-			match Opcode::from(*instruction) {
-				Opcode::Illegal => panic!("Illegal opcode"),
-				Opcode::Halt => false,
-				Opcode::OldLoad => {
-					self.perform_load(instruction)
-				},
-				_ => panic!("Unsupported instruction")
-			}
+			match Instruction::from(*instruction) {
+				Instruction::Illegal => panic!("Illegal opcode"),
+				Instruction::Halt => false,
+                Instruction::Load { value, dest_reg } => self.perform_load(value, dest_reg),
+                Instruction::Add { src1, src2, dest } => self.perform_add(src1, src2, dest),
+            }
+        }
+
+		fn perform_load(&mut self, value: u64, dest_reg: u8) -> bool {
+            match dest_reg {
+                0 => {
+                    self.da = value as i64;
+                    true
+                },
+                1 => {
+                    self.db = value as i64;
+                    true
+                },
+                2 => {
+                    self.dc = value as i64;
+                    true
+                },
+                3 => {
+                    self.dd = value as i64;
+                    true
+                },
+                _ => panic!("Invalid register")
+            }
 		}
 
-		fn perform_load(&mut self, instruction: &u64) -> bool {
-			let operands = instruction >> 10;
-			unimplemented!()
-		}
+        fn perform_add(&mut self, src1: u8, src2: u8, dest: u8) -> bool {
+            unimplemented!()
+        }
 	}
 
 	#[cfg(test)]
@@ -116,20 +158,63 @@ mod kinematic {
 		}
 
 		#[test]
-		fn opcode_from_u64() {
-			let expected = Opcode::Halt;
-			let actual = Opcode::from(0);
+		fn instruction_from_word() {
+            let instruction: Word = 0b000000000000000000000000000000000000000000000000000000_0000000000;
+			let expected = Instruction::Halt;
+			let actual = Instruction::from(instruction);
 			assert_eq!(expected, actual);
 
-			let expected = Opcode::OldLoad;
-			let actual = Opcode::from(1);
+            let instruction: Word = 0b000000000000000000000000000000000000000000000000000000_1000000000;
+			let expected = Instruction::Illegal;
+			let actual = Instruction::from(instruction);
 			assert_eq!(expected, actual);
 
-			let expected = Opcode::Illegal;
-			let actual = Opcode::from(1023);
+            let instruction: Word = 0b0000000000000000000000000000000000001111101000_00001010_0000000001;
+            let expected = Instruction::Load { dest_reg: 10, value: 1000 };
+			let actual = Instruction::from(instruction);
 			assert_eq!(expected, actual);
 		}
-	}
+
+        #[test]
+        fn load_affects_registers() {
+            let instruction: Word = 0b0000000000000000000000000000000000000000001101_00000000_0000000001;
+            let expected_da = 0b1101;
+            let expected_db = 0b0110_0010;
+            let expected_dc = 0b0110_0001;
+            let expected_dd = 0b0011_0010_1001_0100;
+            let program = vec![
+                0b0000000000000000000000000000000000000000001101_00000000_0000000001u64, // load $13, da
+                0b0000000000000000000000000000000000000001100010_00000001_0000000001u64, // load $100, db
+                0b0000000000000000000000000000000000000001100001_00000010_0000000001u64, // load $99, dc
+                0b0000000000000000000000000000000011001010010100_00000011_0000000001u64, // load $12948, db
+            ];
+            let mut vm = Kinematic::new(program);
+            vm.step();
+            assert_eq!(expected_da, vm.da);
+            assert_eq!(0, vm.db);
+            assert_eq!(0, vm.dc);
+            assert_eq!(0, vm.dd);
+            assert_eq!(1, vm.ip);
+            vm.step();
+            assert_eq!(expected_da, vm.da);
+            assert_eq!(expected_db, vm.db);
+            assert_eq!(0, vm.dc);
+            assert_eq!(0, vm.dd);
+            assert_eq!(2, vm.ip);
+            vm.step();
+            assert_eq!(expected_da, vm.da);
+            assert_eq!(expected_db, vm.db);
+            assert_eq!(expected_dc, vm.dc);
+            assert_eq!(0, vm.dd);
+            assert_eq!(3, vm.ip);
+            vm.step();
+            assert_eq!(expected_da, vm.da);
+            assert_eq!(expected_db, vm.db);
+            assert_eq!(expected_dc, vm.dc);
+            assert_eq!(expected_dd, vm.dd);
+            assert_eq!(4, vm.ip);
+	    }       
+    }
 }
 
 fn main() {
