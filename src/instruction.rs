2d12203a877e9e27fb2d99aaae697d3e5ab872b7
Cesar Barata <cesar.adbarata@gmail.com>
2020-01-08 15:04:37 -0300

    Changing indentation from tabs to spaces

--- src/instruction.rs
+++ src/instruction.rs
@@ -4,31 +4,31 @@ use std::convert::From;
 
 #[derive(Debug, PartialEq)]
 pub enum Instruction {
-	Illegal,
-	Halt,
-	Load { value: Word, dest_reg: u8 },
+    Illegal,
+    Halt,
+    Load { value: Word, dest_reg: u8 },
     Add { src1: u8, src2: u8, dest: u8  }
 }
 
 /*
  * For each instruction there is a corresponding parsing function to be used on the
- * implementation for the "From" trait.	Each function has a comment describing the
+ * implementation for the "From" trait. Each function has a comment describing the
  * binary layout of the instruction.
  */
 impl Instruction {
-	const OPCODE_OFFSET: usize = 10;
-	const OPCODE_MASK: u64 = 0b000000_1111111111;
+    const OPCODE_OFFSET: usize = 10;
+    const OPCODE_MASK: u64 = 0b000000_1111111111;
 
-	const LOAD_RANDS_MASK: u64 = 0b00000000_1111111111111111111111111111111111111111111111;
-	const LOAD_DEST_OFFSET: usize = 46;
+    const LOAD_RANDS_MASK: u64 = 0b00000000_1111111111111111111111111111111111111111111111;
+    const LOAD_DEST_OFFSET: usize = 46;
 
-	const ADD_RAND2_OFFSET: usize = 18;
-	const ADD_DEST_OFFSET: usize = 36;
+    const ADD_RAND2_OFFSET: usize = 18;
+    const ADD_DEST_OFFSET: usize = 36;
 
-	/*
-	 * LOAD
-	 *
-	 *    DEST                         VALUE                         OPCODE
+    /*
+     * LOAD
+     *
+     *    DEST                         VALUE                         OPCODE
      * 0b00000000_00000000000000000000000000000000000000000000000(_0000000000)
      * 
      */
@@ -41,7 +41,7 @@ impl Instruction {
     /*
      * ADD
      *
-	 *          DEST               SRC2               SRC1           OPCODE
+     *          DEST               SRC2               SRC1           OPCODE
      * 0b000000000000000000_000000000000000000_000000000000000000(_0000000000)
      */
     fn parse_add(operands: u64) -> Self {
@@ -53,17 +53,17 @@ impl Instruction {
 }
 
 impl From<Word> for Instruction {
-	fn from(instruction: Word) -> Self {
+    fn from(instruction: Word) -> Self {
         let opcode = (instruction & Self::OPCODE_MASK) as u16;
         let operands = (instruction >> Self::OPCODE_OFFSET) as u64;
-		match opcode {
-			0 => Instruction::Halt,
+        match opcode {
+            0 => Instruction::Halt,
             1 => Self::parse_load(operands),
             2 => Self::parse_add(operands),
-			x if x > 1024 => Instruction::Illegal, // we have only 2.pow(10) = 1024 opcode slots
-			_ => Instruction::Illegal              // for still unimplemented instructions
-		}
-	}
+            x if x > 1024 => Instruction::Illegal, // we have only 2.pow(10) = 1024 opcode slots
+            _ => Instruction::Illegal              // for still unimplemented instructions
+        }
+    }
 }
 
 #[cfg(test)]
--- src/kinematic.rs
+++ src/kinematic.rs
@@ -4,50 +4,50 @@ use crate::instruction::Instruction;
 
 pub struct Kinematic {
     // TODO represent writable registers as array
-	da: i64,
-	db: i64,
-	dc: i64,
-	dd: i64,
-	ip: usize,
-	f0: i64,
-	//memory: [u64; MEMORY_SIZE],
-	program: Vec<Word>,
-	running: bool
+    da: i64,
+    db: i64,
+    dc: i64,
+    dd: i64,
+    ip: usize,
+    f0: i64,
+    //memory: [u64; MEMORY_SIZE],
+    program: Vec<Word>,
+    running: bool
 }
 
 impl Kinematic {
-	//const MEMORY_SIZE: usize = 1024;
-	const INITIAL_IP: usize = 0;
+    //const MEMORY_SIZE: usize = 1024;
+    const INITIAL_IP: usize = 0;
 
-	pub fn new(program: Vec<Word>) -> Self {
-		Kinematic {
-			da: 0, db: 0, dc: 0, dd: 0,
-			ip: Self::INITIAL_IP,
-			f0: 0,
-			//memory: [0; MEMORY_SIZE],
-			program,
-			running: false
-		}
-	}
+    pub fn new(program: Vec<Word>) -> Self {
+        Kinematic {
+            da: 0, db: 0, dc: 0, dd: 0,
+            ip: Self::INITIAL_IP,
+            f0: 0,
+            //memory: [0; MEMORY_SIZE],
+            program,
+            running: false
+        }
+    }
 
-	fn fetch_next_instr(&mut self) -> u64 {
-		let instruction = self.program[self.ip];
-		self.ip += 1;
-		instruction
-	}
+    fn fetch_next_instr(&mut self) -> u64 {
+        let instruction = self.program[self.ip];
+        self.ip += 1;
+        instruction
+    }
 
-	fn step(&mut self) -> bool {
-		let instruction = &self.fetch_next_instr();
+    fn step(&mut self) -> bool {
+        let instruction = &self.fetch_next_instr();
 
-		match Instruction::from(*instruction) {
-			Instruction::Illegal => panic!("Illegal opcode"),
-			Instruction::Halt => false,
+        match Instruction::from(*instruction) {
+            Instruction::Illegal => panic!("Illegal opcode"),
+            Instruction::Halt => false,
             Instruction::Load { value, dest_reg } => self.perform_load(value, dest_reg),
             Instruction::Add { src1, src2, dest } => self.perform_add(src1, src2, dest),
         }
     }
 
-	fn perform_load(&mut self, value: u64, dest_reg: u8) -> bool {
+    fn perform_load(&mut self, value: u64, dest_reg: u8) -> bool {
         match dest_reg {
             0 => {
                 self.da = value as i64;
@@ -67,7 +67,7 @@ impl Kinematic {
             },
             _ => panic!("Invalid register")
         }
-	}
+    }
 
     fn perform_add(&mut self, src1: u8, src2: u8, dest: u8) -> bool {
         todo!()
@@ -76,37 +76,37 @@ impl Kinematic {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn default_vm() {
-		let vm = Kinematic::new(vec![0; 0]);
-		assert_eq!(vm.da, 0);
-		assert_eq!(vm.db, 0);
-		assert_eq!(vm.dc, 0);
-		assert_eq!(vm.dd, 0);
-		assert_eq!(vm.ip, Kinematic::INITIAL_IP);
-		assert_eq!(vm.f0, 0);
-		assert_eq!(vm.running, false);
-	}
+    #[test]
+    fn default_vm() {
+        let vm = Kinematic::new(vec![0; 0]);
+        assert_eq!(vm.da, 0);
+        assert_eq!(vm.db, 0);
+        assert_eq!(vm.dc, 0);
+        assert_eq!(vm.dd, 0);
+        assert_eq!(vm.ip, Kinematic::INITIAL_IP);
+        assert_eq!(vm.f0, 0);
+        assert_eq!(vm.running, false);
+    }
 
-	#[test]
-	fn fetch_next_instr() {
-		let program = vec![7, 8, 9];
-		let mut vm = Kinematic::new(program);
+    #[test]
+    fn fetch_next_instr() {
+        let program = vec![7, 8, 9];
+        let mut vm = Kinematic::new(program);
 
-		let instruction = vm.fetch_next_instr();
-		let expected = 7;
-		assert_eq!(expected, instruction);
+        let instruction = vm.fetch_next_instr();
+        let expected = 7;
+        assert_eq!(expected, instruction);
 
-		let instruction = vm.fetch_next_instr();
-		let expected = 8;
-		assert_eq!(expected, instruction);
+        let instruction = vm.fetch_next_instr();
+        let expected = 8;
+        assert_eq!(expected, instruction);
 
-		let instruction = vm.fetch_next_instr();
-		let expected = 9;
-		assert_eq!(expected, instruction);
-	}
+        let instruction = vm.fetch_next_instr();
+        let expected = 9;
+        assert_eq!(expected, instruction);
+    }
 
     #[test]
     fn load_affects_registers() {
