d2cd602151472f2790acff9e4ecfd7353dd0aff9
Cesar Barata <cesar.adbarata@gmail.com>
2020-01-09 14:39:26 -0300

    Moved machine registers from separate fields to an array of registers

--- src/vm.rs
+++ src/vm.rs
@@ -3,27 +3,28 @@ pub type Word = u64;
 use crate::instruction::Instruction;
 
 pub struct Clockwork {
-    // TODO represent writable registers as array
-    da: i64,
-    db: i64,
-    dc: i64,
-    dd: i64,
-    ip: usize,
-    f0: i64,
+    registers: [i64; Self::NUM_REGS],
     //memory: [u64; MEMORY_SIZE],
     program: Vec<Word>,
     running: bool
 }
 
 impl Clockwork {
+    const NUM_REGS: usize = 6;
     //const MEMORY_SIZE: usize = 1024;
+
+    const REG_D0: usize = 0;
+    const REG_D1: usize = 1;
+    const REG_D2: usize = 2;
+    const REG_D3: usize = 3;
+    const REG_IP: usize = 4;
+    const REG_F0: usize = 5;
+
     const INITIAL_IP: usize = 0;
 
     pub fn new(program: Vec<Word>) -> Self {
         Clockwork {
-            da: 0, db: 0, dc: 0, dd: 0,
-            ip: Self::INITIAL_IP,
-            f0: 0,
+            registers: [0, 0, 0, 0, Self::INITIAL_IP as i64, 0],
             //memory: [0; MEMORY_SIZE],
             program,
             running: false
@@ -31,8 +32,8 @@ impl Clockwork {
     }
 
     fn fetch_next_instr(&mut self) -> u64 {
-        let instruction = self.program[self.ip];
-        self.ip += 1;
+        let instruction = self.program[self.registers[Self::REG_IP] as usize];
+        self.registers[Self::REG_IP] += 1;
         instruction
     }
 
@@ -47,26 +48,18 @@ impl Clockwork {
         }
     }
 
+    fn is_reg_writable(index: usize) -> bool {
+        index != Self::REG_F0
+    }
+
     fn perform_load(&mut self, value: u64, dest_reg: u8) -> bool {
-        match dest_reg {
-            0 => {
-                self.da = value as i64;
-                true
-            },
-            1 => {
-                self.db = value as i64;
-                true
-            },
-            2 => {
-                self.dc = value as i64;
-                true
-            },
-            3 => {
-                self.dd = value as i64;
-                true
-            },
-            _ => panic!("Invalid register")
+        if Self::is_reg_writable(dest_reg as usize) {
+            self.registers[dest_reg as usize] = value as i64;
+            self.registers[Self::REG_F0] = 0;
+        } else {
+            self.registers[Self::REG_F0] = 1;
         }
+        true
     }
 
     fn perform_add(&mut self, src1: u8, src2: u8, dest: u8) -> bool {
@@ -81,12 +74,14 @@ mod tests {
     #[test]
     fn default_vm() {
         let vm = Clockwork::new(vec![0; 0]);
-        assert_eq!(vm.da, 0);
-        assert_eq!(vm.db, 0);
-        assert_eq!(vm.dc, 0);
-        assert_eq!(vm.dd, 0);
-        assert_eq!(vm.ip, Clockwork::INITIAL_IP);
-        assert_eq!(vm.f0, 0);
+        assert_eq!(vm.registers, [
+            0i64,
+            0i64,
+            0i64,
+            0i64,
+            Clockwork::INITIAL_IP as i64,
+            0i64
+        ]);
         assert_eq!(vm.running, false);
     }
 
@@ -109,41 +104,51 @@ mod tests {
     }
 
     #[test]
-    fn load_affects_registers() {
-        let expected_da = 0b1101;
-        let expected_db = 0b0110_0100;
-        let expected_dc = 0b0110_0001;
-        let expected_dd = 0b0011_0010_1001_0100;
+    fn load_affects_specific_registers() {
+        let expected_d0 = 0b1101;
+        let expected_d1 = 0b0110_0100;
+        let expected_d2 = 0b0110_0001;
+        let expected_d3 = 0b0011_0010_1001_0100;
+        
         let program = vec![
-            0b00000000_0000000000000000000000000000000000000000001101_0000000001u64, // load $13, da
-            0b00000001_0000000000000000000000000000000000000001100100_0000000001u64, // load $100, db
-            0b00000010_0000000000000000000000000000000000000001100001_0000000001u64, // load $99, dc
-            0b00000011_0000000000000000000000000000000011001010010100_0000000001u64, // load $12948, db
+            0b00000000_0000000000000000000000000000000000000000001101_0000000001u64, // load $13, d0
+            0b00000001_0000000000000000000000000000000000000001100100_0000000001u64, // load $100, d1
+            0b00000010_0000000000000000000000000000000000000001100001_0000000001u64, // load $99, d2
+            0b00000011_0000000000000000000000000000000011001010010100_0000000001u64, // load $12948, d3
         ];
+
         let mut vm = Clockwork::new(program);
+        
         vm.step();
-        assert_eq!(expected_da, vm.da);
-        assert_eq!(0, vm.db);
-        assert_eq!(0, vm.dc);
-        assert_eq!(0, vm.dd);
-        assert_eq!(1, vm.ip);
+        assert_eq!(expected_d0, vm.registers[Clockwork::REG_D0]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D1]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D2]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D3]);
+        assert_eq!(1, vm.registers[Clockwork::REG_IP]);
+        assert_eq!(0, vm.registers[Clockwork::REG_F0]);
+        
         vm.step();
-        assert_eq!(expected_da, vm.da);
-        assert_eq!(expected_db, vm.db);
-        assert_eq!(0, vm.dc);
-        assert_eq!(0, vm.dd);
-        assert_eq!(2, vm.ip);
+        assert_eq!(expected_d0, vm.registers[Clockwork::REG_D0]);
+        assert_eq!(expected_d1, vm.registers[Clockwork::REG_D1]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D2]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D3]);
+        assert_eq!(2, vm.registers[Clockwork::REG_IP]);
+        assert_eq!(0, vm.registers[Clockwork::REG_F0]);
+        
         vm.step();
-        assert_eq!(expected_da, vm.da);
-        assert_eq!(expected_db, vm.db);
-        assert_eq!(expected_dc, vm.dc);
-        assert_eq!(0, vm.dd);
-        assert_eq!(3, vm.ip);
+        assert_eq!(expected_d0, vm.registers[Clockwork::REG_D0]);
+        assert_eq!(expected_d1, vm.registers[Clockwork::REG_D1]);
+        assert_eq!(expected_d2, vm.registers[Clockwork::REG_D2]);
+        assert_eq!(0, vm.registers[Clockwork::REG_D3]);
+        assert_eq!(3, vm.registers[Clockwork::REG_IP]);
+        assert_eq!(0, vm.registers[Clockwork::REG_F0]);
+        
         vm.step();
-        assert_eq!(expected_da, vm.da);
-        assert_eq!(expected_db, vm.db);
-        assert_eq!(expected_dc, vm.dc);
-        assert_eq!(expected_dd, vm.dd);
-        assert_eq!(4, vm.ip);
-    }       
+        assert_eq!(expected_d0, vm.registers[Clockwork::REG_D0]);
+        assert_eq!(expected_d1, vm.registers[Clockwork::REG_D1]);
+        assert_eq!(expected_d2, vm.registers[Clockwork::REG_D2]);
+        assert_eq!(expected_d3, vm.registers[Clockwork::REG_D3]);
+        assert_eq!(4, vm.registers[Clockwork::REG_IP]);
+        assert_eq!(0, vm.registers[Clockwork::REG_F0]);
+    }
 }
